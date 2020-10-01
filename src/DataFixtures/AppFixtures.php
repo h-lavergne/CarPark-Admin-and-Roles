49f9e984193885cb8228ced3519a222e68366689
Lavergne <hugo.lavergne@devinci.fr>
2020-10-01 19:56:53 +0200

    reboot

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\Brand;
 use App\Entity\Car;
 use App\Entity\CarModel;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
--- src/Repository/CarRepository.php
+++ src/Repository/CarRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Car;
 use App\Entity\CarResearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Query;
 
 /**
